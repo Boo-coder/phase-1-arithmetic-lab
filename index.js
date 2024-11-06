0c7e4e6f8c5ffe58bde4c68fd4c2524d3f85a5fe
Boo-coder <Bouna112@yahoo.com>
2024-11-06 00:40:43 -0500

    Art-ph1

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+console.log(multiply);
+console.log(num1);
+console.log(num2);
 
-// Write your code here
+const random = 2
+console.log(Number.isInteger(8));
+
+const num3 = 20;
+const num4 = 16;
+const mod = num3 % num4;
+console.log(mod);
+
+const max = 20;
+console.log(Math.max(7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
